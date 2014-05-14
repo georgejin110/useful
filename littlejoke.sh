cd94dc4ab1371391062707b34dada01f5fe8810a
frjalex <frjalex@gmail.com>
2014-05-14 20:52:40 +0800

    Create littlejoke.sh
    Just a little joke. A script that deletes itself automatically.

---
+++ littlejoke.sh
@@ -0,0 +1,12 @@
+#!/bin/rm
+
+printf:"DELETING..."
+# Self-deleting script.
+
+# Nothing much seems to happen when you run this... except that the file disappears.
+
+WHATEVER=85
+
+echo "This line will never print (betcha!)."
+
+exit $WHATEVER
