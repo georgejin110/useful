f0eab16c32604015ab2d63a224f75034c8f59b0f
frjalex <frjalex@gmail.com>
2014-06-01 17:39:02 +0800

    Fixed some issues
    lastest updates avaliable at http://fermi.theredian.org/d/

--- ipsecauto.sh
+++ ipsecauto.sh
@@ -25,7 +25,7 @@ echo "racoon installation completed."
 
 echo "Now we're configuring it for you!"
 mbanner=hellovpn
-echo "Please enter your preferred connection banner!" $mbanner ; read mbanner
+echo "Please enter your preferred connection banner!"  read mbanner
 echo "your banner"
 echo $mbanner
 cd /etc/racoon/
@@ -35,12 +35,12 @@ $mbanner
 EOF
 gname1=vpn
 
-echo "Your 1st group name is " $gname1 ; read gname1
+echo "Please enter your 1st group name like this " $gname1 ; read gname1
 echo "Your group name is"
 echo $gname1
 
 gsecret1=vpn123
-echo "Your group secret (PSK) is" $gsecret1 ; read gsecret1
+echo "Enter your preferred group secret(PSK) LIKE THIS" $gsecret1 ; read gsecret1
 echo "Your secret is"
 echo $gsecret1
 
@@ -55,23 +55,23 @@ EOF
 #destroy ipsec.conf and rebuild and ask for client ip
 cip=xxx.xxx.xxx.xxx
 echo "Now please enter your server's STATIC IP address."
-echo "The format MUST BE  '%s' . Enter the new value: " $cip ; read cip
+echo "the format must be like xxx.xxx.xxx.xxx now please enter your ip" read cip
 echo "Your server's ip address is"
 echo $cip
 
 dns1=8.8.8.8
-echo "the default dns1 is Google 1(8.8.8.8) If you want to change: " $dns1 ; read dns1
+echo "the default dns1 is Google 1(8.8.8.8) If you want to change: " read dns1
 echo "the dns1 is now:"
 echo $dns1
 
 dns2=8.8.4.4
-echo "the default dns2 is Google 2(8.8.4.4) If you want to change: " $dns2 ; read dns2
+echo "the default dns2 is Google 2(8.8.4.4) If you want to change: " read dns2
 echo "the dns2 is now:"
 echo $dns2
 
 fqdn1=xxx.theredian.org
 echo "please input your servers fqdn the format is like '%s' "
-echo "your fqdn: " $fqdn1 ; read fqdn1
+echo "your fqdn: " read fqdn1
 echo "now the fqdn is"
 echo $fqdn1
 
@@ -144,7 +144,7 @@ echo "this is your ethernet config"
 ifconfig
 
 ipeth=eth0
-echo "Please enter your default ethernet port like '%s' . Enter it : " $ipeth ; read ipeth
+echo "Please enter your default ethernet port like eth0 . Enter it : " read ipeth
 echo "Now your ethernet is"
 echo $ipeth
 
